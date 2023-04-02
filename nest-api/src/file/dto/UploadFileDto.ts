b5f584d819d779392c6827e41af3f353c366692e
gentij <gentijashari5@gmail.com>
2023-04-02 13:46:20 +0200

    feat: swagger

--- nest-api/src/file/dto/UploadFileDto.ts
+++ nest-api/src/file/dto/UploadFileDto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsNumber, IsNumberString } from 'class-validator';
 
 export class UploadFileDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsNumberString()
   brand_id: number;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsNumberString()
   auth_user_id: number;
--- nest-api/src/main.ts
+++ nest-api/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationException } from './common/error/validation-exception';
 import { AllExceptionsFilter } from './common/error/all-exceptions-filter';
 
@@ -8,14 +9,29 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
+
   app.setGlobalPrefix('api');
+
   app.useGlobalFilters(new AllExceptionsFilter());
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
       exceptionFactory: (errors) => new ValidationException(errors),
     }),
   );
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('publiish-api')
+    .setDescription('Description of Publiish API endpoints.')
+    .setVersion('1.0')
+    .addTag('publiish_api')
+    .addServer('http://localhost:3000')
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('swagger', app, swaggerDocument);
+
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
