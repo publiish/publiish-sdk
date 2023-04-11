7baa15f5a7e68208a4d4a0442d60658715a0848d
gentij <gentijashari5@gmail.com>
2023-04-11 15:42:21 +0200

    feat: brand entity

---
+++ nest-api/src/brand/brand.entity.ts
@@ -0,0 +1,43 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { instanceToPlain } from 'class-transformer';
+
+@Entity('brands')
+export class Brand {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 100, nullable: true })
+  brand_name?: string;
+
+  @Column({ length: 200, nullable: true })
+  brand_url?: string;
+
+  @Column('int', { nullable: true })
+  dao_id?: number;
+
+  @Column({ length: 100, nullable: true })
+  email?: string;
+
+  @Column({ length: 100, nullable: true })
+  password?: string;
+
+  @Column({ default: true })
+  write_permission: boolean;
+
+  @Column({ default: true })
+  read_permission: boolean;
+
+  constructor(partial: Partial<File>) {
+    Object.assign(this, partial);
+  }
+
+  toJSON() {
+    return instanceToPlain(this);
+  }
+}
