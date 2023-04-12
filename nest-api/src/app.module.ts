82de39926280163de8246d644051adf3ff4cc6f6
gentij <gentijashari5@gmail.com>
2023-04-12 11:35:59 +0200

    feat: sign up client page

--- nest-api/src/app.module.ts
+++ nest-api/src/app.module.ts
@@ -1,4 +1,6 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { FileModule } from './file/file.module';
 import { DatabaseModule } from './database/database.module';
@@ -10,6 +12,10 @@ import { AuthModule } from './auth/auth.module';
     DatabaseModule,
     AuthModule,
     FileModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../', 'client'),
+      exclude: ['/api/(.*)'],
+    }),
   ],
   providers: [ConfigService],
 })
