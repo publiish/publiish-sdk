9cfc332d183ed3fa9ecfeb975ecee6ad8dc600f6
gentij <gentijashari5@gmail.com>
2023-04-20 15:59:25 +0200

    fix: redirect to signin page when token expire

--- client/src/lib/context/auth/useAuth.ts
+++ client/src/lib/context/auth/useAuth.ts
@@ -4,8 +4,10 @@ import { SigninData } from "@/api/auth/types";
 import { useLocalStorage } from "@/lib/hooks/useLocalStorage";
 import { User, useUser } from "@/lib/hooks/useUser";
 import { signin } from "@/api/auth/Signin";
+import { useRouter } from "next/router";
 
 export const useAuth = () => {
+  const router = useRouter();
   const { user, addUser, removeUser } = useUser();
   const [loading, setLoading] = useState(true);
   const { getItem } = useLocalStorage();
@@ -16,7 +18,7 @@ export const useAuth = () => {
       const token = getItem("token");
 
       if (token) {
-        const user = jwt_decode(token) as User;
+        const user = decodeToken(token) as User;
 
         await addUser(user, token);
       }
@@ -27,13 +29,25 @@ export const useAuth = () => {
   const login = async (body: SigninData) => {
     const { data } = await signin(body);
 
-    const decoded = jwt_decode(data.access_token);
+    const decoded = decodeToken(data.access_token) as User;
 
-    addUser(decoded as User, data.access_token);
+    addUser(decoded, data.access_token);
   };
 
   const logout = () => {
     removeUser();
+  };
+
+  const decodeToken = (token: string) => {
+    const decoded = jwt_decode(token) as User;
+
+    const currentTime = Math.floor(Date.now() / 1000); // convert to seconds
+
+    if (decoded.exp < currentTime) {
+      return router.push("/signin");
+    }
+
+    return decoded;
   };
 
   return { user, login, logout, loading };
